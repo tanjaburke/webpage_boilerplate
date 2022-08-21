d8368c8f43085ef9160fc6440ad8c83598184242
TBU <tbu@novicell.dk>
2022-08-21 11:44:36 +0200

    remove homemade richTextRenderer

--- storyblok/Teaser.vue
+++ storyblok/Teaser.vue
@@ -1,43 +1,18 @@
 <template>
-  <div
-    v-editable="blok"
-    :class="`${blok.class}`"
-  >
-    <h4 v-for="(item, index) in blok.headline.content" :key="index">{{blok.headline.content[index]}}</h4>
+  <div v-editable="blok" :class="`${blok.class}`">
+    <h4 v-for="(item, index) in blok.headline.content" :key="index">
+      {{ blok.headline.content[index] }}
+    </h4>
   </div>
 </template>
- 
-<script lang="ts">
 
+<script lang="ts">
 export default {
-  props: ['blok'],
-  data(){
-      return {
-          myInnerHTML: '',
-      }
+  props: ["blok"],
+  data() {
+    return {
+      myInnerHTML: "",
+    };
   },
-  mounted(){
-      const blokArray = toRaw(this.blok.headline.content);
-      const paragraphs = blokArray.filter(item => item.type === 'paragraph');
-      
-      for (let i = 0; i < blokArray.length; i++) {
-          if (blokArray[i].type === 'paragraph' && blokArray[i].content) {
-              const paragraphObjects = blokArray[i].content;
-              this.buildHTML(paragraphObjects)
-              
-          }
-          
-      }
-  },
-  methods: {
-      buildHTML(objectArray){
-          for (let i = 0; i < objectArray.length; i++) {
-              const testHTML= []
-              const myHTML = objectArray[i].marks
-              console.log('myHTML', myHTML);
-          }
-          
-      }
-  }
-}
+};
 </script>
